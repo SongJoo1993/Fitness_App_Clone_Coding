d7e59a5a034a1a8b2a4537a27d4aba08198d65f3
Song Joo <songjoo29@gmail.com>
2023-09-02 09:07:18 -0400

    Main page being worked

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,17 @@
-import React from 'react'
+import React,{useState} from 'react'
+import {Box} from '@mui/material';
+
+import Excercises from '../components/Excercises'; 
+import SearchExercises from '../components/SearchExercises'; 
+import HeroBanner from '../components/HeroBanner'; 
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <Box>
+      <HeroBanner />
+      <SearchExercises />
+      <Excercises />
+    </Box>
   )
 }
 
